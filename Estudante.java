4e74b32130e7e276105356c46ba15d0ae4069ea8
Lucasflcc <101192302+Lucasflcc@users.noreply.github.com>
2023-04-28 16:38:23 -0300

    corrigido

--- Estudante.java
+++ Estudante.java
@@ -1,14 +1,30 @@
-public class Estudante extends usuario {
+/public class Estudante extends Usuario {
     
-    public Estudante(String user, String cpf, String senha, int idade, String sexo, String email, 
-                     String nomeCartao, String numCartao, String codVerifCartao) {
-        super(user, cpf, senha, idade, sexo, email, nomeCartao, numCartao, codVerifCartao);
-    }
+    //public Estudante(String user, String cpf, String senha, int idade, String sexo, String email, 
+                     //String nomeCartao, String numCartao, String codVerifCartao) {
+        //super(user, cpf, senha, idade, sexo, email, nomeCartao, numCartao, codVerifCartao);
+    //}
     
-    @Override
-    public double bilhete(Filme filme, int quantidade) {
-        double valor = filme.getValor();
-        double total = valor*quantidade*0.5; // aplicando desconto de 50% para estudantes
-        return total;
-    }
+    //@Override
+    //public double bilhete(Filme filme, int quantidade) {
+        //double valor = filme.getValor();
+        //double total = valor*quantidade*0.5; // aplicando desconto de 50% para estudantes
+        //return total;
+    //}
+   
+//}
+
+public class Estudante extends Compra{
+
+	public Estudante(Usuario usuario) {
+		super(usuario);
+
+	}
+	
+	 public double bilhete(Filme filme, int quantidade) {
+		double valor = filme.getValor();
+    	double total = valor*quantidade*0.5;
+    	return total;
+	}
+	
 }
